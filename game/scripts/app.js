2dadda74fba43bed8802af27620390443ac4d1eb
Phoebe E <femmebot@gmail.com>
2018-09-12 17:57:15 -0400

    completed pig game

--- game/scripts/app.js
+++ game/scripts/app.js
@@ -11,13 +11,37 @@ GAME RULES:
 
 let score, roundScore, activePlayer;
 
-score = [0,0];
-roundScore = 0;
-activePlayer = 0;
+// zero out all the things!
+const newGame = () => {
 
-document.querySelector(`#score-${activePlayer}`).textContent = score[activePlayer];
-document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+  score = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
 
+  for (let i in score) {
+  // for (i = 0; i < score.length; i++) {
+    // console.log(value);
+    document.getElementById(`score-${i}`).textContent = score[i];
+    document.getElementById(`current-${i}`).textContent = roundScore;
+    document.querySelector(`.player-${i}-panel`).classList.remove('active');
+    document.querySelector(`.player-${activePlayer}-panel`).classList.add('active');
+  }
+};
+
+// don't show the die initially
+const hideDice = () => document.querySelector('.dice').style.display = 'none';
+
+const switchTurn = () => {
+  hideDice();
+  roundScore = 0;
+  document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+  document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  document.querySelector(`.player-${activePlayer}-panel`).classList.add('active');
+};
+
+newGame();
+hideDice();
 
 // callback function b/c it's called by another function
 // passed into another function as an argument
@@ -33,33 +57,46 @@ document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
 // Let x = document.querySelector(`#current-${activePlayer}`).textContent
 // console.log (x);
 
-// don't show the die initially
-document.querySelector('.dice').style.display = 'none';
-
 // addEventListener takes two parameters: event type and function name
 // document.querySelector('.btn-roll').addEventListener('click', buttonRollHandler )
 
 // using an ES6 anonymous function instead
 document.querySelector('.btn-roll').addEventListener('click', () => {
-  // console.log('button clicked!!!!');
   // 1. Roll die by generating random number 1 - 6
   let diceRoll = (Math.floor(Math.random() * 6) + 1);
-  console.log(diceRoll);
 
-  // 2. Display the result
+  // 2. Display the die image
   const diceDOM = document.querySelector('.dice');
 
   diceDOM.style.display = 'block';
   diceDOM.src = `img/dice-${diceRoll}.png`;
 
   // 3. If the number is not 1, display the score for the round
-  roundScore = Number(document.querySelector(`#current-${activePlayer}`).textContent);
+  roundScore = Number(document.getElementById(`current-${activePlayer}`).textContent);
   if ( diceRoll !== 1 ) {
-    roundScore = roundScore + diceRoll;
+    roundScore += diceRoll;
+    document.getElementById(`current-${activePlayer}`).textContent = roundScore;
   } else {
-    roundScore = 0;
+    switchTurn();
   }
-  document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+});
 
+document.querySelector('.btn-hold').addEventListener('click', () => {
+  console.log('button hold clicked!!!');
+  // 1. add roundScore to #score-1
+  score[activePlayer] += roundScore;
+  // 2. Update UI
+  console.log(`Score: ${ score[activePlayer] } Type: ${ typeof(score[activePlayer]) }` );
+  document.getElementById(`score-${activePlayer}`).textContent = score[activePlayer];
+  // 3. Check if player won the Game
+  if ( score[activePlayer] >= 100 ) {
+    alert ('You won!!');
+  } else {
+    switchTurn();
+  };
 
+});
+
+document.querySelector('.btn-new').addEventListener('click', () => {
+  newGame();
 });
